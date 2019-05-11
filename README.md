45de900094e71b52f0bf93a8a6c7d6d7e9a7ab6c
supermirooo <supermirooo@hotmail.com>
2019-05-11 15:21:13 +0200

    Restructure

---
+++ README.md
@@ -0,0 +1,30 @@
+# EazyMind
+
+**EazyMind** provides **ai-as-a-service** for **abstractive text summarizaion** , you simply provide your
+1. Sentence to be summarized
+2. Api key [from free registeration on eazymind website ](http://eazymind.herokuapp.com/arabic_sum)
+
+-------------------------------------------------
+## Insatllation :
+1. Regsiter a Free account on [EazyMind](http://eazymind.herokuapp.com/arabic_sum)
+2. ```pip install eazymind```
+
+-------------------------------------------------
+## Usage :
+```
+from nlp.eazysum import Summarizer
+
+#---key from eazymind website---
+key = "xxxxxxxxxxxxxxxxxxxxx"
+
+#---sentence to be summarized---
+sentence = """(CNN)The White House has instructed former
+    White House Counsel Don McGahn not to comply with a subpoena
+    for documents from House Judiciary Chairman Jerry Nadler, 
+    teeing up the latest in a series of escalating oversight 
+    showdowns between the Trump administration and congressional Democrats."""
+    
+summarizer = Summarizer(key)
+print(summarizer.run(sentence))
+```
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'eazymind',         # How you named your package folder (MyLib)
+  packages = ['eazymind'],   # Chose the same as "name"
+  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'AI Library providing ai-as-a-service for abstractive text summarization , just your text to be summarized and key obtained from free registeration on eazymind and a summary would be provided , get key from http://eazymind.herokuapp.com/arabic_sum',   # Give a short description about your library
+  author = 'amr zaki',                   # Type in your name
+  author_email = 'theamrzaki@hotmail.com',      # Type in your E-Mail
+  url = 'https://github.com/theamrzaki/eazymind',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['nlp', 'text', 'summarization' , 'ai' ,'natural language processing'],   # Keywords that define your package best
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 2.7', 
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
